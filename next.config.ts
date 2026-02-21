6741df64dbb3dbcdf446b68c784b0c601d1cb307
Cosmo Rodrigues <cosmo.rsilva@gmail.com>
2026-02-20 22:19:10 -0300

    feat: create tailwind settings

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  transpilePackages: ["antd", "@ant-design/icons", "@ant-design/cssinjs"],
+}
 
 export default nextConfig;
